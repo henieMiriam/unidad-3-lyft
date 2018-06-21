18c60c3770c4f3ba3c439f16b148a027029fa350
henieMiriam <henie_yo@hotmail.com>
2018-06-21 16:07:28 -0500

    se agrega la vista de inicio con dos botones

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+//en la perte bsuperior se deben incluir declaraciones,
+
+
+
+/*esta parte siempre va al final del codigo , icluye :
+llamadas a funciones
+declaracion de EVENTOS
+llamadas del DOM
+sintaxis jquery 
+*/
+$(document).ready(function(){
+
+});
